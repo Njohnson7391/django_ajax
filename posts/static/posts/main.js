ee9901d1e867bb67597c62e2ab17cef0fa0f5253
Neethu Johnson <njohnson7391@conestogac.on.ca>
2024-04-04 22:20:38 -0700

    Added evenlistener for close button

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -15,7 +15,9 @@ const alertBox = document.getElementById('alert-box')
 
 const url = window.location.href
 
-console.log('csrf', csrf[0].value)
+const dropzone = document.getElementById('my-dropzone')
+const addBtn = document.getElementById('add-btn')
+const closeBtns = [...document.getElementsByClassName('add-modal-close')]
 
 const getCookie =(name) => {
     let cookieValue = null;
@@ -164,9 +166,9 @@ postForm.addEventListener('submit', e=>{
                 </div>
             `)
           likeUnlikePosts()
-          $('#addPostModal').modal('hide')
+        //   $('#addPostModal').modal('hide')
           handleAlerts('success', 'New post added!')
-          postForm.reset() 
+        //   postForm.reset() 
 
         },
         error: function(error){
@@ -175,5 +177,16 @@ postForm.addEventListener('submit', e=>{
         }
     })
 })
+
+addBtn.addEventListener('click', ()=>{
+     dropzone.classList.remove('not-visible')
+})
+
+closeBtns.forEach(btn=> btn.addEventListener('click', ()=>{
+    postForm.reset()
+    if (!dropzone.classList.contains('not-visible')){
+        dropzone.classList.add('not-visible')
+    } 
+}))
 
 getData()
