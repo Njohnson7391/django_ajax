ece81dc2724afecc2394590d7b0abe9a166c6068
Neethu Johnson <njohnson7391@conestogac.on.ca>
2024-04-04 19:44:18 -0700

    Added codes for delete, update, alert, back buttons

---
+++ posts/static/posts/detail.js
@@ -0,0 +1,59 @@
+console.log('hello world detail')
+
+const postBox = document.getElementById('post-box')
+const alertBox = document.getElementById('alert-box')
+const backBtn = document.getElementById('back-btn')
+const updateBtn = document.getElementById('update-btn')
+const deleteBtn = document.getElementById('delete-btn')
+
+
+
+const spinnerBox = document.getElementById('spinner-box')
+
+const titleInput = document.getElementById('id_title')
+const bodyInput = document.getElementById('id_body')
+
+const csrf = document.getElementsByName('csrfmiddlewaretoken')
+
+backBtn.addEventListener('click', ()=>{
+    history.back()
+})
+
+$.ajax({
+    type: 'GET',
+    url: url,
+    success: function(response){
+        console.log(response)
+        const data = response.data
+
+        if (data.logged_in !== data.author){
+            console.log('different')
+        }else {
+            console.log('the same')
+            updateBtn.classList.remove('not-visible')
+            deleteBtn.classList.remove('not-visible')
+        }
+
+        const titleEl = document.createElement('h3')
+        titleEl.setAttribute('class', 'mt-3')
+        titleEl.setAttribute('id', 'title')
+
+        const bodyEl = document.createElement('p')
+        bodyEl.setAttribute('class', 'mt-1')
+        bodyEl.setAttribute('id', 'body')
+
+        titleEl.textContent = data.title
+        bodyEl.textContent = data.body
+
+        postBox.appendChild(titleEl)
+        postBox.appendChild(bodyEl)
+
+        titleInput.value = data.title
+        bodyInput.value = data.body
+
+        spinnerBox.classList.add('not-visible')
+    },
+    error: function(error){
+        console.log(error)
+    }
+})
