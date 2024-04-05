bf26396ca6de072aaa11222882b1ab3ea9418dac
Neethu Johnson <njohnson7391@conestogac.on.ca>
2024-04-05 15:19:15 -0700

    Tested some issues in the POst UI and rectified the erroes

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -85,24 +85,23 @@ const getData = () =>{
             data.forEach(el => {
                 postBox.innerHTML+= `
                 <div class="card mb-2">
-                
-                <div class="card-body">
-                  <h5 class="card-title">${el.title}</h5>
-                  <p class="card-text">${el.body}</p>
+                  <div class="card-body">
+                     <h5 class="card-title">${el.title}</h5>
+                     <p class="card-text">${el.body}</p>
                   </div>
                   <div class="card-footer">
-                  <div class="row">
+                   <div class="row">
                      <div class="col-2">
                      <a href="${url}${el.id}" class="btn btn-primary">Details</a>
-                  </div>
-                  <div class="col-2">
-                  <form class="like-unlike-forms" data-form-id="${el.id}">
-                     
-                    <button href="#" class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
-                  </form>
+                      </div>
+                   <div class="col-2">
+                    <form class="like-unlike-forms" data-form-id="${el.id}">
+                      <button class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
+                    </form>
                     </div>
                 </div>
               </div>
+            </div>  
                 `
             });
             likeUnlikePosts()
@@ -131,6 +130,7 @@ loadBtn.addEventListener('click', ()=>{
     getData()
 })
 
+let newPostId = null
 postForm.addEventListener('submit', e=>{
     e.preventDefault()
 
@@ -144,26 +144,28 @@ postForm.addEventListener('submit', e=>{
         },
         success: function(response){
             console.log(response)
+            newPostId = response.id
             postBox.insertAdjacentHTML('afterbegin', `
-                    <div class="card mb-2">
+            <div class="card mb-2">
                         
-                    <div class="card-body">
+                <div class="card-body">
                     <h5 class="card-title">${response.title}</h5>
                     <p class="card-text">${response.body}</p>
-                    </div>
-                    <div class="card-footer">
+                </div>
+                <div class="card-footer">
                     <div class="row">
                         <div class="col-2">
-                        <a href="#" class="btn btn-primary">Details</a>
-                    </div>
+                        <a href="${url}${response.id}" class="btn btn-primary">Details</a>
+                        </div>
                     <div class="col-2">
-                    <form class="like-unlike-forms" data-form-id="${response.id}">
+                      <form class="like-unlike-forms" data-form-id="${response.id}">
                         
-                        <button href="#" class="btn btn-primary" id="like-unlike-${response.id}">Like (0)</button>
-                    </form>
+                        <button class="btn btn-primary" id="like-unlike-${response.id}">Like (0)</button>
+                       </form>
                         </div>
                     </div>
                 </div>
+            </div>  
             `)
           likeUnlikePosts()
         //   $('#addPostModal').modal('hide')
@@ -187,6 +189,22 @@ closeBtns.forEach(btn=> btn.addEventListener('click', ()=>{
     if (!dropzone.classList.contains('not-visible')){
         dropzone.classList.add('not-visible')
     } 
+    const myDropzone = Dropzone.forElement("#my-dropzone")
+    myDropzone.removeAllFiles(true)
 }))
+
+Dropzone.autoDiscover = false
+const myDropzone = new Dropzone('#my-dropzone', {
+    url: 'upload/',
+    init: function(){
+        this.on('sending', function(file, xhr, formData){
+            formData.append('csrfmiddlewaretoken', csrftoken)
+            formData.append('new_post_id', newPostId)
+        })
+    },
+    maxFiles: 5,
+    maxFilesize: 4,
+    acceptedFiles: '.png, .jpg, .jpeg'
+})
 
 getData()
