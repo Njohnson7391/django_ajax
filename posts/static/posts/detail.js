e97dca246f5a80c10c93e6be410557b95bd0b2ba
Neethu Johnson <njohnson7391@conestogac.on.ca>
2024-04-04 20:07:18 -0700

    Added code for update and delete in detail.js

--- posts/static/posts/detail.js
+++ posts/static/posts/detail.js
@@ -6,7 +6,12 @@ const backBtn = document.getElementById('back-btn')
 const updateBtn = document.getElementById('update-btn')
 const deleteBtn = document.getElementById('delete-btn')
 
+const url = window.location.href + "data/"
+const updateUrl = window.location.href + "update/"
+const deleteUrl = window.location.href + "delete/"
 
+const updateForm = document.getElementById('update-form')
+const deleteForm = document.getElementById('delete-form')
 
 const spinnerBox = document.getElementById('spinner-box')
 
@@ -14,6 +19,7 @@ const titleInput = document.getElementById('id_title')
 const bodyInput = document.getElementById('id_body')
 
 const csrf = document.getElementsByName('csrfmiddlewaretoken')
+
 
 backBtn.addEventListener('click', ()=>{
     history.back()
@@ -56,4 +62,52 @@ $.ajax({
     error: function(error){
         console.log(error)
     }
+})
+
+updateForm.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    const title = document.getElementById('title')
+    const body = document.getElementById('body')
+
+    $.ajax({
+        type: 'POST',
+        url: updateUrl,
+        data: {
+            'csrfmiddlewaretoken': csrf[0].value,
+            'title': titleInput.value,
+            'body': bodyInput.value,
+        },
+        success: function(response){
+            console.log(response)
+            handleAlerts('success', 'post has been updated')
+            title.textContent = response.title
+            body.textContent = response.body
+        },
+        error: function(error){
+            console.log(error)
+        }
+    })
+
+
+})
+
+
+deleteForm.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    $.ajax({
+        type: 'POST',
+        url: deleteUrl,
+        data: {
+            'csrfmiddlewaretoken': csrf[0].value,
+        },
+        success: function(response){
+           window.location.href = window.location.origin
+           localStorage.setItem('title', titleInput.value)
+        },
+        error:function(error){
+            console.log(error)
+        }
+    })
 })
