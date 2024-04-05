ed3f089b7fe6fbcfe29b8e18b714c72b5d4db2e5
Neethu Johnson <njohnson7391@conestogac.on.ca>
2024-04-04 20:39:45 -0700

    Added ajax method for details

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,13 +1,17 @@
 console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
-
+const postBox = document.getElementById('posts-box')
 
 const spinnerBox = document.getElementById('spinner-box')
 const loadBtn = document.getElementById('load-btn')
 const endBox = document.getElementById('end-box')
 
-
+const postForm = document.getElementById('post-form')
+const title = document.getElementById('id_title')
+const body = document.getElementById('id_body')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')
+const alertBox = document.getElementById('alert-box')
 
 const url = window.location.href
 
@@ -30,6 +34,11 @@ const getCookie =(name) => {
 }
 const csrftoken = getCookie('csrftoken');
 
+const deleted = localStorage.getItem('title')
+if (deleted){
+    handleAlerts('danger', `deleted "${deleted}"`)
+    localStorage.clear()
+}
 
 const likeUnlikePosts = ()=>{
     const likeUnlikeForms = [...document.getElementsByClassName('like-unlike-forms')]
@@ -56,3 +65,115 @@ const likeUnlikePosts = ()=>{
         })
     }))
 }
+
+let visible = 3
+
+const getData = () =>{
+
+    $.ajax({
+        type:'GET',
+        url: `/data/${visible}/`,
+        success: function(response){
+            console.log(response)
+            const data =response.data
+            setTimeout(()=>{
+                spinnerBox.classList.add('not-visible')
+    
+            console.log (data)
+            data.forEach(el => {
+                postBox.innerHTML+= `
+                <div class="card mb-2">
+                
+                <div class="card-body">
+                  <h5 class="card-title">${el.title}</h5>
+                  <p class="card-text">${el.body}</p>
+                  </div>
+                  <div class="card-footer">
+                  <div class="row">
+                     <div class="col-2">
+                     <a href="${url}${el.id}" class="btn btn-primary">Details</a>
+                  </div>
+                  <div class="col-2">
+                  <form class="like-unlike-forms" data-form-id="${el.id}">
+                     
+                    <button href="#" class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
+                  </form>
+                    </div>
+                </div>
+              </div>
+                `
+            });
+            likeUnlikePosts()
+            }, 100)
+            console.log(response.size)
+            if(response.size==0){
+                endBox.textContent = 'No posts added yet..'
+            }
+            else if(response.size <= visible){
+                loadBtn.classList.add('not-visible')
+                endBox.textContent = 'No more posts to load..'
+            }
+
+        },
+        error:function(error){
+            console.log('error', error)
+        }
+    
+    })
+
+}
+
+loadBtn.addEventListener('click', ()=>{
+    spinnerBox.classList.remove('not-visible')
+    visible+=3
+    getData()
+})
+
+postForm.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    $.ajax({
+        type: 'POST',
+        url: '',
+        data:{
+            'csrfmiddlewaretoken': csrf[0].value,
+            'title': title.value,
+            'body': body.value
+        },
+        success: function(response){
+            console.log(response)
+            postBox.insertAdjacentHTML('afterbegin', `
+                    <div class="card mb-2">
+                        
+                    <div class="card-body">
+                    <h5 class="card-title">${response.title}</h5>
+                    <p class="card-text">${response.body}</p>
+                    </div>
+                    <div class="card-footer">
+                    <div class="row">
+                        <div class="col-2">
+                        <a href="#" class="btn btn-primary">Details</a>
+                    </div>
+                    <div class="col-2">
+                    <form class="like-unlike-forms" data-form-id="${response.id}">
+                        
+                        <button href="#" class="btn btn-primary" id="like-unlike-${response.id}">Like (0)</button>
+                    </form>
+                        </div>
+                    </div>
+                </div>
+            `)
+          likeUnlikePosts()
+          $('#addPostModal').modal('hide')
+          handleAlerts('success', 'New post added!')
+          postForm.reset() 
+
+        },
+        error: function(error){
+            console.log(error)
+            handleAlerts('danger', 'oops..something went wrong')
+        }
+    })
+})
+
+getData()
