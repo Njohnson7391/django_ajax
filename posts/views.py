bad3bf3c976f7d75c7698ff1b079a2c7fcc6a8a9
Neethu Johnson <njohnson7391@conestogac.on.ca>
2024-04-04 19:17:51 -0700

    Added load_post_data_view for loading the post

--- posts/views.py
+++ posts/views.py
@@ -43,4 +43,25 @@ def post_detail(request, pk):
         'form': form,
     } 
 
-    return render(request, 'posts/detail.html', context)    
+    return render(request, 'posts/detail.html', context)   
+
+def load_post_data_view(request, num_posts):
+        visible = 3
+        upper = num_posts
+        lower = upper - visible
+        size = Post.objects.all().count()
+
+        qs = Post.objects.all()
+        data = []
+        for obj in qs:
+            item = {
+                'id': obj.id,
+                'title': obj.title,
+                'body': obj.body,
+                'liked': 'True' if request.user in obj.liked.all() else 'False',  
+                'count': obj.like_count,  
+                'author': obj.author.user.username  
+            }
+            data.append(item)
+
+        return JsonResponse({'data': data[lower:upper], 'size': size})     
